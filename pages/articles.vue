052667faa73c5d40806b0753924e42ffcf2cb1a4
Heejong Ahn <heejongahn@gmail.com>
2017-07-06 22:11:09 +0900

    Add articles page

---
+++ pages/articles.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container">
+    <h1>글</h1>
+    <div :key="article.id" v-for="article in articles">
+      <nuxt-link :to="`/article/${article.id}`">
+        <h2>{{article.title}}</h2>
+        <div>2017.07.03</div>
+      </nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      articles: [
+        { id: 1, title: '나의 버건디 8면체' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
