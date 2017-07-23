345dcb5b62a4d843bf05da954ff214592b3aed9c
Heejong Ahn <heejongahn@gmail.com>
2017-07-23 14:58:54 +0900

    Stop rotation on mouse hover

--- components/Octahedron.vue
+++ components/Octahedron.vue
@@ -5,7 +5,17 @@
 <script>
 export default {
   mounted: function () {
-    render()
+    const containerBoundingRect = this.$el.getBoundingClientRect()
+    const mousePosition = new Vector2()
+
+    this.$el.addEventListener('mousemove', e => {
+      mousePosition.set(
+        e.clientX,
+        e.clientY
+      )
+    })
+
+    render(containerBoundingRect, mousePosition)
   }
 }
 
@@ -16,13 +26,46 @@ import {
   PointLight,
   MeshLambertMaterial,
   Mesh,
-  OctahedronGeometry
+  OctahedronGeometry,
+  Vector2,
+  Raycaster
 } from 'three'
 
-function render () {
+function getNormalizedMousePosition (windowPosition, mousePosition) {
+  const width = windowPosition.right - windowPosition.left
+  const height = windowPosition.bottom - windowPosition.top
+
+  const centerX = windowPosition.left + (width / 2)
+  const centerY = windowPosition.top + (height / 2)
+
+  const normalizedX = (mousePosition.x - centerX) / (width / 2)
+  const normalizedY = (mousePosition.y - centerY) / (height / 2)
+
+  return {
+    x: Math.min(1, Math.max(-1, normalizedX)),
+    y: Math.min(1, Math.max(-1, normalizedY))
+  }
+}
+
+function render (containerBoundingRect, mousePosition) {
   const WIDTH = 200
   const HEIGHT = WIDTH
   const RADIUS = WIDTH / 6
+
+  const POSITION = {
+    top: containerBoundingRect.top,
+    bottom: containerBoundingRect.top + HEIGHT,
+    left: containerBoundingRect.left,
+    right: containerBoundingRect.left + WIDTH
+  }
+
+  const VELOCITY = {
+    x: 0,
+    y: 0,
+    z: 0
+  }
+
+  const raycaster = new Raycaster()
 
   const VIEW_ANGLE = 20
   const ASPECT = WIDTH / HEIGHT
@@ -73,10 +116,19 @@ function render () {
   scene.add(octahedron)
 
   function update () {
+    raycaster.setFromCamera(getNormalizedMousePosition(POSITION, mousePosition), camera)
+    const intersections = raycaster.intersectObject(octahedron)
+
     const speed = Math.random() / 20
-    octahedron.rotation.x += speed
-    octahedron.rotation.y += speed
-    octahedron.rotation.z += speed
+    VELOCITY.x = speed
+    VELOCITY.y = speed
+    VELOCITY.z = speed
+
+    if (intersections.length === 0) {
+      octahedron.rotation.x += VELOCITY.x
+      octahedron.rotation.y += VELOCITY.y
+      octahedron.rotation.z += VELOCITY.z
+    }
     renderer.render(scene, camera)
     requestAnimationFrame(update)
   }
