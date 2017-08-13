ba69e4e27ae47808429558a118b7ab79614ae906
Heejong Ahn <heejongahn@gmail.com>
2017-08-13 13:30:27 +0900

    Add components/Painting for painting article

--- pages/palette/fb-shimmer/index.vue
+++ pages/palette/fb-shimmer/index.vue
@@ -1,27 +1,27 @@
 <template>
   <article class="container">
-    <h1 :class="$style.title">Facebook Shimmer</h1>
-    <div :class="$style.shimmerWrap">
-      <fb-shimmer />
-    </div>
-    <p :class="$style.description">
-      Facebook's famous shimmering effect for placeholders.
-      The trick here is, you don't actually apply animations on several gray bars.
-      Rather, you apply linear-gradient animation on the rectangular background and ‘mask’ the rest part using white rectangle (inspecting with DevTool might help you understand what's going on).
-      So implementing this effect requires labor, not fancy library or anything.
-      <div>Kudos to the smart person who first thought of this idea 👏</div>
-    </p>
-    <a target="_blank" href="https://github.com/heejongahn/ahnheejong.name/blob/master/pages/palette/fb-shimmer/_FBShimmer.vue">
-      <small>view source code for this component</small>
-    </a>
+    <painting title="Facebook Shimmer" githubHref="fb-shimmer/_FBShimmer.vue">
+      <div slot="component" :class="$style.shimmerWrap">
+        <fb-shimmer />
+      </div>
+      <div slot="description" :class="$style.description">
+        Facebook's famous shimmering effect for placeholders.
+        The trick here is, you don't actually apply animations on several gray bars.
+        Rather, you apply linear-gradient animation on the rectangular background and ‘mask’ the rest part using white rectangle (inspecting with DevTool might help you understand what's going on).
+        So implementing this effect requires labor, not fancy library or anything.
+        <p :class="$style.kudos">Kudos to the smart person who first thought of this idea 👏</p>
+      </div>
+    </painting>
   </article>
 </template>
 
 <script>
+import Painting from '~/components/Painting'
 import FBShimmer from './_FBShimmer'
 
 export default {
   components: {
+    'painting': Painting,
     'fb-shimmer': FBShimmer
   }
 }
@@ -31,10 +31,5 @@ export default {
 .shimmerWrap {
   max-width: 424px;
   width: 100%;
-}
-
-.description {
-  max-width: 600px;
-  line-height: 1.8;
 }
 </style>
